94bae7af69382815c3af3f003b610043e854ab0b
Marcus Deh <deh.marcus@outlook.de>
2019-02-14 12:14:02 +0100

    Initial commit

---
+++ bot_utils/__init__.py
@@ -0,0 +1,52 @@
+import socket
+import requests
+import typing
+name = "bot_utils"
+
+
+class Logger:
+    """ Sends messages to the logging rest api """
+
+    def __init__(self, bot_id: str, host: str, port: str):
+        self.bot_id = bot_id
+        self.host = host
+        self.port = port
+        self.error = 'error'
+        self.info = 'info'
+
+    def log_error(self, error: typing.TypeVar):
+        """ Log an Error Message """
+        message = 'an error occured: ' + str(error)
+        self._log(message, self.error)
+
+    def log_info(self, message: typing.TypeVar):
+        self._log(message, self.info)
+
+    def _log(self, message, log_type):
+        payload = {
+            'bot_id': self.bot_id,
+            'message': str(message)
+        }
+        requests.post('http://' + self.host + ':' + str(self.port) + '/logger/' + log_type, json=payload)
+
+
+class ConnectivityChecker:
+    """ Checks availability of a given host. reports error via provided error reporting function """
+
+    def __init__(self, error_reporter: typing.Callable[[str], typing.NoReturn]):
+        self.error_reporter = error_reporter
+
+    def is_available(self, host: str, port: str) -> bool:
+        server_socket = None
+        try:
+            server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            server_result = server_socket.connect_ex((host, port))
+            if server_result == 0:
+                return True
+            else:
+                return False
+        except (socket.gaierror, socket.herror, socket.error) as err:
+            self.error_reporter('error checking for server availability :: ' + str(err))
+        finally:
+            if server_socket is not None:
+                server_socket.close()
---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="bot_utils",
+    version="0.0.3",
+    author="Marcus Deh",
+    author_email="deh.marcus@outlook.de",
+    description="package containing methods and classes",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/theweekendgeek/bot_utils",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=['requests'],
+)

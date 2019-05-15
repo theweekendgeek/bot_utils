3b8e8998a19711a79d4c10b1416078a61793dfe7
Marcus Deh <deh.marcus@outlook.de>
2019-05-15 15:20:00 +0200

    fix: added more rechtsformen

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="bot_utils",
-    version="0.0.6",
+    version="0.0.7",
     author="Marcus Deh",
     author_email="deh.marcus@outlook.de",
     description="package containing methods and classes",
